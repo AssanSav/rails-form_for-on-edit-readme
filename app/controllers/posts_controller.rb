1fe9e2b8e1a52c447e7de15eb560fd95fc681bec
Assane Savadogo <assane.savadogo81@gmail.com>
2019-11-17 15:28:22 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,29 +3,33 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
-	def show
-		@post = Post.find(params[:id])
-	end
-
+	
 	def new
 		@post = Post.new
 	end
-
+	
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
-	  @post.save
-	  redirect_to post_path(@post)
+		@post = Post.new
+		@post.title = params[:title]
+		@post.description = params[:description]
+		@post.save
+		redirect_to post_path(@post)
 	end
-
+	
+	def show
+		@post = Post.find(params[:id])
+	end
+	
 	def edit
 		@post = Post.find(params[:id])
 	end
-
+	
 	def update
+		#binding.pry
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	#   @post.update(title: params[:post][:title], description: params[:post][:description])
+	@post.update(params.require(:post).permit(:title, :description))
 	  redirect_to post_path(@post)
 	end
+
 end
